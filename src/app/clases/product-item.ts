ee68fa9636945e60f4ec8c6fc805f324437346fa
jesus <jesus.martinez.gonzalez1993@gmail.com>
2023-05-30 12:52:35 +0200

    creada clase productoItem

---
+++ src/app/clases/product-item.ts
@@ -0,0 +1,18 @@
+export class ProductItem {
+
+    idProducto:number = 0;
+    nombre:string = "";
+    categoria:string = "";
+
+
+    constructor(idProducto:number,nombre:string,categoria:string){
+
+        this.idProducto = idProducto;
+        this.nombre = nombre;
+        this.categoria = categoria;
+
+    }
+
+
+    
+}
