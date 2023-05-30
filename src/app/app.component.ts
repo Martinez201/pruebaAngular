d7291196e317556ccbcd5b7945f79bcfbaca30da
jesus <jesus.martinez.gonzalez1993@gmail.com>
2023-05-30 13:26:45 +0200

    añadiendo producto a una lista

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { ProductItem } from './clases/product-item';
 
 @Component({
   selector: 'app-root',
@@ -9,4 +10,21 @@ export class AppComponent {
   title = 'prueba';
 
   producItemsId = [1,2,3,4,5,6]; 
+
+  listaProductos:Array<ProductItem> = new Array<ProductItem>
+
+
+  annadirProductos(){
+
+    this.listaProductos.push(new ProductItem(1,"Lavadora","lavadora Bosh"));
+    this.listaProductos.push(new ProductItem(1,"afeitadora","afeitadora philips"));
+    this.listaProductos.push(new ProductItem(1,"secadora","secadora balay"));
+    this.listaProductos.push(new ProductItem(1,"termomix","termomix Bosh"));
+    this.listaProductos.push(new ProductItem(1,"batidora","batidora Bosh"));
+    this.listaProductos.push(new ProductItem(1,"Ordenador","Portatil HP"));
+
+  }
+
 }
+
+
